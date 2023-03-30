483bc87bd1563772d2fe299146f64a8629517f33
MarcoValeri <marcovaleri@hotmail.it>
2023-03-30 11:06:47 +0100

    Add lowerCase method

--- main.go
+++ main.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strings"
+
 	util "goutil/util"
 )
 
 func main() {
-	util.LowerCase()
-	fmt.Println("Hello GO")
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Select the util that you want to use")
+	fmt.Println("Enter the number of the util that you want to use")
+	fmt.Println("1 - LowerCase method: convert a string in the lowercase")
+	fmt.Println("0 - Exit: zero terminates the program - you can also terminate the program with 'exit' word")
+
+	fmt.Print("Enter your option: ")
+	option, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("You have selected:", option)
+
+	switch strings.Trim(option, "\r\n") {
+	case "1":
+		util.LowerCase()
+	case "0":
+		fmt.Println("Exit")
+	case "exit":
+		fmt.Println("Exit")
+	}
+
 }
--- util/lowerCase.go
+++ util/lowerCase.go
@@ -1,7 +1,27 @@
 package util
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
 func LowerCase() {
-	fmt.Println("LowerCase")
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Enter the string that you want to lower case: ")
+		getString, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		lowerCaseString := strings.ToLower(getString)
+		fmt.Println(lowerCaseString)
+
+		if strings.Trim(getString, "\r\n") == "0" || strings.Trim(getString, "\r\n") == "exit" {
+			fmt.Println("Exit")
+			break
+		}
+	}
 }
