f719556c9138e021a77710565232ae52d604c29f
Marco Valeri <marco.valeri@cherrythinking.com>
2024-01-18 17:11:47 +0000

    Add upprcase util

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func main() {
 	fmt.Println("Select the util that you want to use")
 	fmt.Println("Enter the number of the util that you want to use")
 	fmt.Println("1 - LowerCase method: convert a string in the lowercase")
+	fmt.Println("2 - UpperCase method: convert a string in the uppercase")
 	fmt.Println("0 - Exit: zero terminates the program - you can also terminate the program with 'exit' word")
 
 	fmt.Print("Enter your option: ")
@@ -28,6 +29,8 @@ func main() {
 	switch strings.Trim(option, "\r\n") {
 	case "1":
 		util.LowerCase()
+	case "2":
+		util.UpperCase()
 	case "0":
 		fmt.Println("Exit")
 	case "exit":
--- util/upperCase.go
+++ util/upperCase.go
@@ -8,17 +8,17 @@ import (
 	"strings"
 )
 
-func LowerCase() {
+func UpperCase() {
 	for {
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Println("Enter '0' or 'exit' to end the program")
-		fmt.Print("Enter the string that you want to lower case: ")
+		fmt.Print("Enter the string that you want to upper case: ")
 		getString, err := reader.ReadString('\n')
 		if err != nil {
 			log.Fatal(err)
 		}
-		lowerCaseString := strings.ToLower(getString)
-		fmt.Println(lowerCaseString)
+		upperCaseString := strings.ToUpper(getString)
+		fmt.Println(upperCaseString)
 
 		if strings.Trim(getString, "\r\n") == "0" || strings.Trim(getString, "\r\n") == "exit" {
 			fmt.Println("Exit")
